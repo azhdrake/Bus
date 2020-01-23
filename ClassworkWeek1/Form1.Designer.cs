b712ed6a33d5dda5cf53c695797659b2787a1e70
B33 <my@email.com>
2020-01-23 09:43:27 -0600

    Add project files.

---
+++ ClassworkWeek1/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace ClassworkWeek1
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.CalcButt = new System.Windows.Forms.Button();
+      this.BusTripNumber = new System.Windows.Forms.TextBox();
+      this.TotalBox = new System.Windows.Forms.TextBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.fuckThisShitImOut = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // CalcButt
+      // 
+      this.CalcButt.Location = new System.Drawing.Point(388, 326);
+      this.CalcButt.Name = "CalcButt";
+      this.CalcButt.Size = new System.Drawing.Size(180, 43);
+      this.CalcButt.TabIndex = 0;
+      this.CalcButt.Text = "Yes Touchy";
+      this.CalcButt.UseVisualStyleBackColor = true;
+      this.CalcButt.Click += new System.EventHandler(this.calcButt_Click);
+      // 
+      // BusTripNumber
+      // 
+      this.BusTripNumber.Location = new System.Drawing.Point(388, 88);
+      this.BusTripNumber.Name = "BusTripNumber";
+      this.BusTripNumber.Size = new System.Drawing.Size(180, 31);
+      this.BusTripNumber.TabIndex = 1;
+      this.BusTripNumber.TextChanged += new System.EventHandler(this.BusTripNumber_TextChanged);
+      // 
+      // TotalBox
+      // 
+      this.TotalBox.Location = new System.Drawing.Point(388, 206);
+      this.TotalBox.Name = "TotalBox";
+      this.TotalBox.ReadOnly = true;
+      this.TotalBox.Size = new System.Drawing.Size(180, 31);
+      this.TotalBox.TabIndex = 2;
+      this.TotalBox.TextChanged += new System.EventHandler(this.TotalBox_TextChanged);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(94, 93);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(208, 25);
+      this.label1.TabIndex = 3;
+      this.label1.Text = "Number of Bus Trips";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(99, 211);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(122, 25);
+      this.label2.TabIndex = 4;
+      this.label2.Text = "Total Spent";
+      // 
+      // fuckThisShitImOut
+      // 
+      this.fuckThisShitImOut.Location = new System.Drawing.Point(99, 326);
+      this.fuckThisShitImOut.Name = "fuckThisShitImOut";
+      this.fuckThisShitImOut.Size = new System.Drawing.Size(162, 42);
+      this.fuckThisShitImOut.TabIndex = 5;
+      this.fuckThisShitImOut.Text = "I\'m Out!";
+      this.fuckThisShitImOut.UseVisualStyleBackColor = true;
+      this.fuckThisShitImOut.Click += new System.EventHandler(this.fuckThisShitImOut_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.fuckThisShitImOut);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.TotalBox);
+      this.Controls.Add(this.BusTripNumber);
+      this.Controls.Add(this.CalcButt);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button CalcButt;
+    private System.Windows.Forms.TextBox BusTripNumber;
+    private System.Windows.Forms.TextBox TotalBox;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Button fuckThisShitImOut;
+  }
+}
+
---
+++ ClassworkWeek1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassworkWeek1
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void calcButt_Click(object sender, EventArgs e)
+    {
+      double busRate = 2.50;
+      int trips = 0;
+      if (Int32.TryParse(BusTripNumber.Text, out trips))
+      {
+        double total = busRate * trips;
+        TotalBox.Text = "$" + Convert.ToString(total);
+      }
+      else
+      {
+        MessageBox.Show("Please enter an interager", "Error");
+      }
+    }
+
+    private void TotalBox_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void BusTripNumber_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void fuckThisShitImOut_Click(object sender, EventArgs e)
+    {
+      this.Close();
+    }
+  }
+}
